a3dfbfbaa29e6471b4969bd86bb68d40b966e511
Hamin <hamin2633@naver.com>
2023-03-28 02:54:02 +0900

    프론트 : 메인화면, 피드 업로드 모달, 로그인, 회원가입 백엔드 : DB 내용으로 메인화면 구성, DB에 새로운 피드 데이터 추가, 화면에 보여주기, 사용자 정보 모델 만들기

--- content/views.py
+++ content/views.py
@@ -1,3 +1,29 @@
+from uuid import uuid4
+import os
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Feed
+from Minstagram.settings import MEDIA_ROOT
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id') # select * from content_feed;
+        return render(request, "Minstagram/main.html", context=dict(feeds=feed_list))
 
-# Create your views here.
+class UploadFeed(APIView):
+    def post(self, request):
+        # 파일 불러오는 부분
+        file = request.FILES['file']
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        image = uuid_name
+        content = request.data.get('content')
+        user_id = request.data.get('user_id')
+        profile_image = request.data.get('profile_image')
+
+        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image, like_count=0)
+        return Response(status=200)
