4c4354e7c7b01c7375b11c4031aa0f819b4573f4
Hamin <hamin2633@naver.com>
2023-03-30 02:14:37 +0900

    인스타 구현 완료 에러 나는 부분 : 1. 프로필 사진 가져오기 안됨 2. 프로필에서 북마크 표시한 게시물 보여주기 안됨 3. 피드 추가 모달 창 선택 후 닫을 시 스크롤 사라짐 4. 파드 내린 상태에서 추가 버튼 누르면 맨 위에 모달 창이 생기면서 멈춤

--- user/views.py
+++ user/views.py
@@ -1,5 +1,12 @@
+import os
+from uuid import uuid4
+
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import User
+from django.contrib.auth.hashers import make_password
+from Minstagram.settings import MEDIA_ROOT
 
 
 # Create your views here.
@@ -8,10 +15,63 @@ class Join(APIView):
         return render(request, "user/join.html")
 
     def post(self, request):
-        pass
+        email = request.data.get('email', None)
+        nickname = request.data.get('nickname', None)
+        name = request.data.get('name', None)
+        password = request.data.get('password', None)
+
+        User.objects.create(email=email,
+                            nickname=nickname,
+                            name=name,
+                            password=make_password(password),
+                            profile_image="default_profile.jpg")
+
+        return Response(status=200)
+
+
 class Login(APIView):
     def get(self, request):
         return render(request, "user/login.html")
 
     def post(self, request):
-        pass
+        email = request.data.get('email', None)
+        password = request.data.get('password', None)
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            return Response(status=400, data=dict(message="회원 정보가 잘못되었습니다."))
+
+        if user.check_password(password):
+            # 로그인을 했다. 세션 0r 쿠키
+            request.session['email'] = email
+            return Response(status=200)
+        else:
+            return Response(status=400, data=dict(message="회원 정보가 잘못되었습니다."))
+
+class Logout(APIView):
+    def get(self, request):
+        request.session.flush()
+        return render(request, "user/login.html")
+
+
+class UploadProfile(APIView):
+    def post(self, request):
+        # 파일 불러오는 부분
+        file = request.FILES['file']
+        email = request.data.get('email')
+
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        profile_image = uuid_name
+
+        user = User.objects.filter(email=email).first()
+
+        user.profile_image = profile_image
+        user.save()
+
+        return Response(status=200)
